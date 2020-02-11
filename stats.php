b852fe67c6fd1aebdadb496fd6c26029d5fab129
Jacek Tomasiak <jacek.tomasiak@gmail.com>
2020-02-11 15:08:24 +0100

    added tags support to stats app

--- stats.php
+++ stats.php
@@ -6,6 +6,15 @@ $json = file_get_contents('php://input');
 $post = json_decode($json, true);
 // TODO: handle json decode errors
 
+function value_type($value) {
+    return is_numeric($value) ? 'DOUBLE PRECISION' : 'CHARACTER VARYING';
+}
+
+function ensure_column($table, $column, $type) {
+    $res = pg_query("ALTER TABLE $table ADD COLUMN IF NOT EXISTS $column $type") or die('Query failed: ' . pg_last_error());
+    pg_free_result($res);
+}
+
 // write data
 if ($post !== null) {
     $table = pg_escape_identifier($_GET['table']);
@@ -15,25 +24,42 @@ if ($post !== null) {
     // default to now
     $ts = $post['time'] ?? time();
 
+    // extract tags
+    $tags = $post['tags'] ?? array();
+    unset($post['tags']);
+    // make tags query
+    $tags_query = '';
+    $tags_cols = '';
+    $tags_vals = '';
+    if (!empty($tags)) {
+        foreach ($tags as $tag => $value) {
+            // make sure tag column exists
+            ensure_column($table, $tag, value_type($value));
+            // avoid SQL injections
+            $tag = pg_escape_identifier($tag);
+            $value = pg_escape_literal($value);
+
+            $tags_query .= " AND $tag=$value";
+            $tags_cols .= ", $tag";
+            $tags_vals .= ", $value";
+        }
+    }
+
     foreach ($post as $column => $value) {
         if ($column == 'time') continue;
-        // basic value type detection
-        $type = is_numeric($value) ? 'DOUBLE PRECISION' : 'CHARACTER VARYING';
+        // make sure requested column exists
+        ensure_column($table, $column, value_type($value));
         // avoid SQL injections
         $column = pg_escape_identifier($column);
         $value = pg_escape_literal($value);
 
-        // make sure requested column exists
-        $res = pg_query("ALTER TABLE $table ADD COLUMN IF NOT EXISTS $column $type") or die('Query failed: ' . pg_last_error());
-        pg_free_result($res);
-
         // try updating first (if requested time row exists)
-        $res = pg_query("UPDATE $table SET $column=$value WHERE time=to_timestamp($ts)") or die('Query failed: ' . pg_last_error());
+        $res = pg_query("UPDATE $table SET $column=$value WHERE time=to_timestamp($ts) $tags_query") or die('Query failed: ' . pg_last_error());
         $updated = (pg_affected_rows($res) > 0);
         pg_free_result($res);
         // insert if update didn't do anything
         if (!$updated) {
-            $res = pg_query("INSERT INTO $table (time, $column) VALUES (to_timestamp($ts), $value)") or die('Query failed: ' . pg_last_error());
+            $res = pg_query("INSERT INTO $table (time, $column $tags_cols) VALUES (to_timestamp($ts), $value $tags_vals)") or die('Query failed: ' . pg_last_error());
             pg_free_result($res);
         }
     }

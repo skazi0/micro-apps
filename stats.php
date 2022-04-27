b574f075135ef343dc3859fcbb7889689e94a10f
Jacek Tomasiak <jacek.tomasiak@gmail.com>
2022-04-28 00:40:38 +0200

    Fix ensure_column and error handling
    The ensure_column doesn't fail if not owner of table and not trying
    to alter.
    In case of errors return HTTP500 with die() message.

--- stats.php
+++ stats.php
@@ -2,25 +2,36 @@
 $pw = explode(':', file_get_contents('/etc/nginx/statspasswd'));
 $db = pg_connect("host=$pw[0] dbname=stats user=$pw[1] password=$pw[2]") or die('Could not connect: ' . pg_last_error());
 
-$table = pg_escape_identifier($_GET['table']);
+$table_bare = $_GET['table'];
+$table = pg_escape_identifier($table_bare);
 
 $json = file_get_contents('php://input');
 $post = json_decode($json, true);
 // TODO: handle json decode errors
 
+function http_die($msg) {
+    http_response_code(500);
+    die($msg);
+}
+
 function value_type($value) {
     return is_numeric($value) ? 'DOUBLE PRECISION' : 'CHARACTER VARYING';
 }
 
-function ensure_column($table, $column, $type) {
-    $res = pg_query("ALTER TABLE $table ADD COLUMN IF NOT EXISTS $column $type") or die('Query failed: ' . pg_last_error());
+function ensure_column($db, $table, $column, $type) {
+    $meta = pg_meta_data($db, $table) or http_die('Meta query failed: ' . pg_last_error());
+    if (array_key_exists($column, $meta))
+        return;
+
+    $table = pg_escape_identifier($table);
+    $res = pg_query("ALTER TABLE $table ADD COLUMN IF NOT EXISTS $column $type") or http_die('Query failed: ' . pg_last_error());
     pg_free_result($res);
 }
 
 // write data
 if ($post !== null) {
     // make sure requested table exists
-    $res = pg_query("CREATE TABLE IF NOT EXISTS $table (time TIMESTAMP WITH TIME ZONE NOT NULL)") or die('Query failed: ' . pg_last_error());
+    $res = pg_query("CREATE TABLE IF NOT EXISTS $table (time TIMESTAMP WITH TIME ZONE NOT NULL)") or http_die('Query failed: ' . pg_last_error());
     pg_free_result($res);
     // default to now
     $ts = $post['time'] ?? time();
@@ -35,7 +46,7 @@ if ($post !== null) {
     if (!empty($tags)) {
         foreach ($tags as $tag => $value) {
             // make sure tag column exists
-            ensure_column($table, $tag, value_type($value));
+            ensure_column($db, $table_bare, $tag, value_type($value));
             // avoid SQL injections
             $tag = pg_escape_identifier($tag);
             $value = pg_escape_literal($value);
@@ -49,18 +60,18 @@ if ($post !== null) {
     foreach ($post as $column => $value) {
         if ($column == 'time') continue;
         // make sure requested column exists
-        ensure_column($table, $column, value_type($value));
+        ensure_column($db, $table_bare, $column, value_type($value));
         // avoid SQL injections
         $column = pg_escape_identifier($column);
         $value = pg_escape_literal($value);
 
         // try updating first (if requested time row exists)
-        $res = pg_query("UPDATE $table SET $column=$value WHERE time=to_timestamp($ts) $tags_query") or die('Query failed: ' . pg_last_error());
+        $res = pg_query("UPDATE $table SET $column=$value WHERE time=to_timestamp($ts) $tags_query") or http_die('Query failed: ' . pg_last_error());
         $updated = (pg_affected_rows($res) > 0);
         pg_free_result($res);
         // insert if update didn't do anything
         if (!$updated) {
-            $res = pg_query("INSERT INTO $table (time, $column $tags_cols) VALUES (to_timestamp($ts), $value $tags_vals)") or die('Query failed: ' . pg_last_error());
+            $res = pg_query("INSERT INTO $table (time, $column $tags_cols) VALUES (to_timestamp($ts), $value $tags_vals)") or http_die('Query failed: ' . pg_last_error());
             pg_free_result($res);
         }
     }
@@ -79,7 +90,7 @@ if ($post !== null) {
         $table = pg_escape_identifier(sprintf("%s_%04d", strtolower($_GET['table']), $row[0]));
     }
 
-    $res = pg_query("SELECT $column FROM $table WHERE time < $time ORDER BY time DESC LIMIT 1") or die('Query failed: ' . pg_last_error());
+    $res = pg_query("SELECT $column FROM $table WHERE time < $time ORDER BY time DESC LIMIT 1") or http_die('Query failed: ' . pg_last_error());
     $row = pg_fetch_row($res);
     pg_free_result($res);
     print $row[0];
